b9d6b34f8c3dc2ec982c1f3b3bd84b8bbbd22bcf
Ken Yaggel <chenyag40@gmail.com>
2023-01-01 08:32:20 +0200

    changes: added flow claculation, gets an array for every two images

--- nematics/Ken/workflow.py
+++ nematics/Ken/workflow.py
@@ -1,5 +1,6 @@
 # import torch
 # import torchvision
+import os
 import scipy as sp
 import numpy as np
 import pandas as pd
@@ -35,12 +36,14 @@ class image_series:
         folder_path,
         window_size = 40, 
         resize_image = None,
-        save = False ) -> None:
+        save_defects = False,
+        save_flow = False
+        ) -> None:
         
         self.path = folder_path
         self.window_size = window_size
-        self.save = save
-        
+        self.save = save_defects
+        self.save_flow = save_flow
         self.resize_image = resize_image
         self.fetch_data()
 
@@ -82,9 +85,9 @@ class image_series:
             
             name = img.name
             plus, minus= img.detect_minus_plus()
-            if self.save:
+            if self.save_defects:
             
-            #save single image 
+            #save_defects single image 
 
                 plus[['charge', 'x','y','ang1']].to_csv(self.path + r"\\csv\\" +name + '_PlusHalf.csv')
                 minus[['charge', 'x','y','ang1','ang2','ang3']].to_csv(
@@ -97,7 +100,7 @@ class image_series:
             plushalf = pd.concat([plushalf, plus])
             minushalf = pd.concat([minushalf, minus])
 
-        if self.save:
+        if self.save_defects:
             plushalf[['from_img','charge', 'x','y','ang1']].to_csv(self.path + r"\csv\PlusHalf.csv")
             minushalf[['from_img','charge', 'x','y','ang1','ang2','ang3']].to_csv(
                 self.path + r"\csv\MinusHalf.csv")
@@ -111,6 +114,54 @@ class image_series:
         return f"workflow : \n  number of images = {len(self.images)}"
 
 
+    def get_flow(self) -> np.array:
+
+        """
+        calculates the optical flow for image_list and returns as an np.array
+        saves if save_flow == True to the previous directory at folder valocity
+        doesn't add the directory
+        """
+        if self.save_flow:
+            path_to_save = os.path.normpath(
+                self.path + os.sep + os.pardir
+                ) + '\\velocity\\' #go back one dir and go to velocity dir
+
+        #create a big array that will contain all the velocities
+        img_size = self.images[0].img.shape
+        num_of_velocity_images = len(self.images) -1
+        big_arr = np.empty(shape = (num_of_velocity_images,*img_size,2))
+        
+        
+        for (i,im1), im2 in zip(enumerate(self.images[:-1]),self.images[1:]):
+
+            # methods = ['cv2.TM_CCOEFF']
+            # pad = 200
+
+            img1 = im1.img
+            img2 = im2.img
+            
+            flow = cv2.calcOpticalFlowFarneback(img1,img2, None, 0.5, 3, 
+                winsize=61, iterations=3, poly_n=5, poly_sigma=1.2, flags=0)
+            
+            big_arr[i] = flow
+
+
+            #save a single image mat 
+            if self.save_flow:
+                name = 'velocity_from_' +im1.name.split('.')[0] + '.mat' #
+                sp.io.savemat(path_to_save+name, dict(u=flow[:,:,0], v=flow[:,:,1]))
+            
+        #save the big array as mat
+        # if self.save_flow:
+        #     # sp.io.savemat(path_to_save + 'big_array.mat',dict(u=big_arr[:,:,:,0], v=big_arr[:,:,:,1]))
+        #     np.save(path_to_save + 'big_array.npy',big_arr)
+
+
+
+        return None#big_arr
+            
+
+        
 
 
 
@@ -177,4 +228,6 @@ class image:
         
         
         return plushalf, minushalf
+
+    
                     
