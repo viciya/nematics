1797744184f29221068bd2f04b14eeb349d732d2
victor <victory@efa-technologies.com>
2023-01-09 11:04:59 +0200

    **

--- nematics/test_cellpose.py
+++ nematics/test_cellpose.py
@@ -3,16 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from cellpose import plot
+import os
 # %matplotlib qt
 
-
 im_path = r"C:\Users\USER\Downloads\HBEC\s2_250_500\Trans__s2_0024.tif"
-img = cv2.imread(im_path)[:,:,0]
-
-seg = np.load(r"C:\Users\USER\Downloads\HBEC\s2_250_500\Trans__s2_0024_seg.npy", allow_pickle=True).item()
+os.path.splitext(f)[0] 
+seg = np.load(r"C:\Users\USER\Downloads\HBEC\s2_250_500\masks\Trans__s2_0024_seg.npy", allow_pickle=True).item()
 masks= seg['masks'].squeeze()
+rgb = plot.mask_rgb(masks, colors=None)
+
+
+img = cv2.imread(im_path)[:,:,0]
 plt.imshow(img)
-plt.imshow(masks, cmap='Pastel1', alpha=0.3)
+plt.imshow(rgb, alpha=0.4)
+
+# plot.mask_overlay(
+#     io.imread(r"C:\Users\USER\Downloads\HBEC\s2_250_500\Trans__s2_0024.tif"),  
+#     io.imread(r"C:\Users\USER\Downloads\HBEC\s2_250_500\masks\Trans__s2_0024_cp_masks.png"), 
+#     colors=None)
 # %% 
 # area in pixels
 plt.figure(figsize=(6,6))
