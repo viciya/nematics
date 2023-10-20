b4efaaac9484e5b43675d72c168a96a1e8f92304
viciya <victoryashunsky@gmail.com>
2023-10-20 08:38:14 +0300

    fuctions moved out of journals

---
+++ nematics/defect_functions/average_flows.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from natsort import natsorted
+import sys
+import pandas as pd
+from scipy.ndimage import rotate, gaussian_filter
+import pickle 
+from scipy.stats import circmean, circstd, sem
+
+sys.path.append('../vasco_scripts')  # add the relative path to the folder
+from defects import *  # import the module from the folder
+
+
+from scipy.ndimage import rotate, gaussian_filter
+
+def divergence_npgrad(flow):
+    flow = np.swapaxes(flow, 0, 1)
+    Fx, Fy = flow[:, :, 0], flow[:, :, 1]
+    dFx_dx = np.gradient(Fx, axis=0)
+    dFy_dy = np.gradient(Fy, axis=1)
+    return (dFx_dx + dFy_dy).T
+
+def curl_npgrad(flow):
+    flow = np.swapaxes(flow, 0, 1)
+    Fx, Fy = flow[:, :, 0], flow[:, :, 1]
+    dFx_dy = np.gradient(Fx, axis=1)
+    dFy_dx = np.gradient(Fy, axis=0)
+    curl = dFy_dx - dFx_dy
+    return curl.T
+
+def crop(img, center, width, height):
+    ulx, uly = max(int(center[0] - width//2), 0), max(int(center[1] - height//2), 0)
+    lrx, lry = min(int(center[0] + width//2), img.shape[1]), min(int(center[1] + height//2), img.shape[0])
+    new_center = ((lrx-ulx)/2 , (lry-uly)/2)
+    return img[uly:lry,ulx:lrx], new_center
+
+def rotate_vector(vector, angle):
+    '''rotate vectors'''
+    x = vector[0] * np.cos(angle) - vector[1] * -np.sin(angle)
+    y = vector[0] * -np.sin(angle) + vector[1] * np.cos(angle)
+    return [x, y]    
+
+def rotate_flow_field(flow, angle):
+    '''rotate flow field'''
+    uv_rot = rotate_vector(flow, angle)
+    u = rotate(uv_rot[0], angle * 180/np.pi)
+    v = rotate(uv_rot[1], angle * 180/np.pi)
+    return [u, v]  
+
+def defect_flow_frame_average(img1,img2, df_frame, defect_type="up", box=(300,300), filt=1):
+    width, height = box[0], box[1]
+    width1, height1 = int(width/2**.5), int(height/2**.5)
+    flow = cv2.calcOpticalFlowFarneback(img1,img2, None, 0.5, 3, 
+        winsize=15, iterations=3, poly_n=5, poly_sigma=1.2, flags=0)
+    flow = gaussian_filter(flow, sigma=filt)
+
+    if defect_type=="up":
+        df = df_frame[df_frame.fuse_up].copy()
+    elif defect_type=="down":
+        df = df_frame[~df_frame.fuse_up].copy()   
+    else:
+        df = df_frame.copy()
+
+
+    u_frame = np.zeros((height1, width1), dtype=np.float32)
+    v_frame = u_frame
+    count = 0
+
+    x,y,th = ['xm', 'ym', 'angm1'] if defect_type=="minus" else ['xp', 'yp', 'angp1']
+
+    for i in range(len(df[x])):
+        try:
+            # center at defect position
+            cnt = (int(df[x].iloc[i]), int(df[y].iloc[i]))
+            #1 crop each component of velocity field
+            
+            # image_crop = crop(255-img_clahe, cnt, width, height)[0] *** image
+            u,_ = crop(flow[:,:,0], cnt, width, height)
+            v,_ = crop(flow[:,:,1], cnt, width, height)
+
+            #2 rotate velocity field (1. rotate vectors 2. rotate positions) 
+            # image_rot = rotate(image_crop, df["angp1"].iloc[i] * 180/np.pi) *** image
+            uv_rot = rotate_flow_field((u,v), df[th].iloc[i])
+
+            #3 crop again to smaller box (box**0.5)
+            cnt_crop = uv_rot[0].shape[1]/2, uv_rot[0].shape[0]/2
+            # image_rot_crop = crop(image_rot, cnt_crop, width1, height1)[0] *** image
+            u_frame = u_frame + crop(uv_rot[0], cnt_crop, width1, height1)[0]
+            v_frame = v_frame + crop(uv_rot[1], cnt_crop, width1, height1)[0]
+            count += 1 
+        except:
+            pass
+        #      break
+
+    if count:
+        return u_frame/count, v_frame/count, count
---
+++ nematics/defect_functions/defect_pairs.py
@@ -0,0 +1,161 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+from natsort import natsorted
+import sys
+import pandas as pd
+from scipy.ndimage import rotate, gaussian_filter
+import pickle 
+from scipy.stats import circmean, circstd, sem
+
+
+sys.path.append('../vasco_scripts')  # add the relative path to the folder
+from defects import *  # import the module from the folder
+
+
+def analyze_defects(img, sigma=15):
+    # Calculate mgrid
+    yy, xx = np.mgrid[0:img.shape[0], 0:img.shape[1]]
+    
+    # Calculate orientation analysis
+    ori, coh, E = orientation_analysis(img, sigma)
+    
+    # Compute topological charges
+    k = compute_topological_charges(-ori, int_area='cell', origin='lower')
+    
+    # Localize defects
+    defects = localize_defects(k, x_grid=xx, y_grid=yy)
+    
+    # Compute defect orientation
+    compute_defect_orientations(-ori, defects, method='interpolation', x_grid=xx[0,:], y_grid=yy[:,0], interpolation_radius=5, min_sep=1)
+    
+    # Filter defects by charge
+    plushalf = defects[defects['charge']==.5]
+    minushalf = defects[defects['charge']==-.5]
+    
+    return ori, plushalf, minushalf
+
+from scipy import spatial
+def center_pairs(Xlong, Xshort):
+    '''find indexes of Xshort in Xlong'''
+    tree = spatial.KDTree(Xlong)
+    return tree.query(Xshort)   
+
+def plot_orientation_analysis(img, ori, df_plus, df_minus, ax=False, colors=["lawngreen","r","b"], alpha=.8, imshow=True):
+    '''If color is False skip do not plot the feature'''
+    alpha_half, scale_half, s = alpha, 25 , 15
+    y, x = np.mgrid[0:img.shape[0], 0:img.shape[1]]
+
+    if not ax:
+        fig, ax = plt.subplots(1,1, figsize=(6,6))
+
+    if imshow:
+        # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        # img_clahe = clahe.apply(img)
+        # ax.imshow(img_clahe, cmap="gray")  
+        ax.imshow(np.zeros_like(img, dtype=np.float32), cmap="gray")
+
+    if colors[0]:
+        ax.quiver(x[::s,::s], y[::s,::s],
+            np.cos(ori)[::s,::s], np.sin(ori)[::s,::s], 
+            headaxislength=0, headwidth=0, headlength=0, 
+            color=colors[0], scale=60, pivot='mid', alpha=.7)
+  
+    if colors[1]:
+        ax.plot(df_plus['x'], df_plus['y'],'o',markersize=6, alpha=alpha_half, color=colors[1])
+        ax.quiver(df_plus['x'], df_plus['y'], 
+            np.cos(df_plus['ang1']), -np.sin(df_plus['ang1']), 
+            headaxislength=0, headwidth=0, headlength=0, color=colors[1], scale=scale_half, alpha=alpha_half)
+
+    if colors[2]:
+        ax.plot(df_minus['x'], df_minus['y'],'o',markersize=6, alpha=alpha_half, color=colors[2])
+        for j in range(3):
+            ax.quiver(df_minus['x'], df_minus['y'], 
+                np.cos(df_minus['ang'+str(j+1)]), -np.sin(df_minus['ang'+str(j+1)]), 
+                headaxislength=0, headwidth=0, headlength=0, color=colors[2], scale=scale_half+10, alpha=alpha_half)
+
+import sys,time,random
+def progressBar(count_value, total, suffix=''):
+    bar_length = 100
+    filled_up_Length = int(round(bar_length* count_value / float(total)))
+    percentage = round(100.0 * count_value/float(total),1)
+    bar = '=' * filled_up_Length + '-' * (bar_length - filled_up_Length)
+    sys.stdout.write('[%s] %s%s ...%s\r' %(bar, percentage, '%', suffix))
+    sys.stdout.flush()
+
+
+def roll_func(what,basis,window,func,*args,**kwargs):
+    '''https://stackoverflow.com/questions/14300768/pandas-rolling-computation-with-window-based-on-values-instead-of-counts'''
+    
+    '''see more examples:
+    https://stackoverflow.com/questions/14313510/how-to-calculate-rolling-moving-average-using-python-numpy-scipy/14314054#14314054'''
+    # from scipy.stats import circmean, circstd
+    #note that basis must be sorted in order for this to work properly     
+    indexed_what = pd.Series(what.values,index=basis.values)
+    def applyToWindow(val):
+        # using slice_indexer rather that what.loc [val:val+window] allows
+        # window limits that are not specifically in the index
+        indexer = indexed_what.index.slice_indexer(val-window,val+window,1)
+        chunk = indexed_what.iloc[indexer]
+        return func(chunk,*args,**kwargs)
+    rolled = basis.apply(applyToWindow)
+    return rolled
+
+def plot_rolling_average(df,ax, what_key, basis_key, show=True, win=15, color="red", avfunc=circmean, stdfunc=circstd, *args,**kwargs):
+    rad2deg = 180/np.pi if avfunc==circmean else 1.
+    df = df.sort_values(by=basis_key)
+    df[what_key+"_ave"] = roll_func(df[what_key], df[basis_key], win, avfunc, *args,**kwargs)*rad2deg
+    df[what_key+"_std"] = roll_func(df[what_key], df[basis_key], win, stdfunc)*rad2deg
+    df[what_key+"_count"] = roll_func(1.*df[what_key].abs(), df[basis_key], win, np.sum)
+    if show:
+        ax.plot(df[basis_key], df[what_key+"_ave"], "-", color=color, alpha=.6, linewidth=3)
+        ax.fill_between(df[basis_key], 
+                        df[what_key+"_ave"]-df[what_key+"_std"]/df[what_key+"_count"]**.5, 
+                        df[what_key+"_ave"]+df[what_key+"_std"]/df[what_key+"_count"]**.5, 
+                        color=color, alpha=.2) 
+    return df[[basis_key, what_key+"_ave", what_key+"_std", what_key+"_count"]] 
+
+
+def msd_from_df(df,xlabel,ylabel,tlabel,id_label,minimal_track=3):
+    def msd(x,y):
+        return np.cumsum(np.diff(x))**2 + np.cumsum(np.diff(y))**2
+
+    msds = []
+    for id in df[id_label].unique():
+        idx = (df[id_label]==id)
+        df_ = df[idx].copy()
+        if idx.sum()>minimal_track:
+            t = df_[tlabel].to_numpy()
+            x = df_[xlabel].to_numpy()
+            y = df_[ylabel].to_numpy()
+            msds.append(pd.DataFrame({"FRAME": t[1:]-t[0], 'MSD':msd(x,y)}).dropna())            
+    msd_df = pd.concat(msds)
+    return msd_df[msd_df["FRAME"]>0]
+
+def ang_msd_from_df(df,xlabel,tlabel,id_label,period=2*np.pi, minimal_track=3):
+    def msd(x):
+        x = np.unwrap(x, period=period)
+        return np.cumsum(np.diff(x))**2
+
+    msds = []
+    for id in df[id_label].unique():
+        idx = (df[id_label]==id)
+        df_ = df[idx].copy()
+        if idx.sum()>minimal_track:
+            t = df_[tlabel].to_numpy()
+            x = df_[xlabel].to_numpy()
+            msds.append(pd.DataFrame({"FRAME": t[1:]-t[0], 'MSD':msd(x)}).dropna())   
+    msd_df = pd.concat(msds)
+    return msd_df[msd_df["FRAME"]>0]  
+
+def equlalize_trajectories(plus_minus_df, p_idx, m_idx):
+    ''' makes sure that positive (p_idx) and negative (m_idx) defect trajectory are equal
+    according to "FRAME" number
+    if missing frame for one of them it removed with dropna()
+    '''
+    df1 = plus_minus_df[["TRACK_ID","FRAME","x_img1","y_img1","ang1"]][p_idx]
+    df1.set_index('FRAME', inplace=True)
+    df2 =  plus_minus_df[["TRACK_ID","FRAME", "x_img1","y_img1", "ang1","ang2","ang3" ]][m_idx]
+    df2.set_index('FRAME', inplace=True)
+    df_ = pd.concat([df1, df2], axis=1).dropna()
+    return df_.set_axis(["plus_id","xp","yp","angp1", "min_id","xm","ym", "angm1","angm2","angm3"], axis=1)
